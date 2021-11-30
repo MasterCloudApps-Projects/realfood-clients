d228f3c37a5d3bfd7ba1e69950f17e556bc1c012
cristoflop <cristofer.lc.99@gmail.com>
2021-11-30 02:46:46 +0100

    Add delete client use case

--- src/main/kotlin/es/urjc/realfood/clients/application/DeleteClient.kt
+++ src/main/kotlin/es/urjc/realfood/clients/application/DeleteClient.kt
@@ -1,7 +1,22 @@
 package es.urjc.realfood.clients.application
 
-class DeleteClient {
+import es.urjc.realfood.clients.domain.exception.ClientNotFoundException
+import es.urjc.realfood.clients.domain.repository.ClientRepository
+import org.springframework.stereotype.Service
 
+@Service
+class DeleteClient(
+    private val clientRepository: ClientRepository
+) {
 
+    operator fun invoke(request: DeleteClientRequest) {
+        val client = clientRepository
+            .findById(request.id) ?: throw ClientNotFoundException("Client not found")
+        clientRepository.delete(client)
+    }
 
 }
+
+data class DeleteClientRequest(
+    val id: String
+)
