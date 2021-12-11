5c06212f1f1961ca59fd5f939e51e39cf37f7aa2
cristoflop <cristofer.lc.99@gmail.com>
2021-12-11 01:34:43 +0100

    Add clear cart use case

---
+++ src/main/kotlin/es/urjc/realfood/clients/application/ClearCart.kt
@@ -0,0 +1,30 @@
+package es.urjc.realfood.clients.application
+
+import es.urjc.realfood.clients.domain.ClientId
+import es.urjc.realfood.clients.domain.repository.CartRepository
+import org.springframework.stereotype.Service
+import javax.persistence.EntityNotFoundException
+import javax.transaction.Transactional
+
+@Service
+@Transactional
+class ClearCart(
+    private val cartRepository: CartRepository
+) {
+
+    operator fun invoke(request: ClearCartRequest) {
+        val clientId = ClientId(request.clientId)
+
+        val cart = cartRepository.findByClientId(clientId)
+            ?: throw EntityNotFoundException("Cart not found")
+
+        cart.items.clear()
+
+        cartRepository.save(cart)
+    }
+
+}
+
+data class ClearCartRequest(
+    val clientId: String
+)
