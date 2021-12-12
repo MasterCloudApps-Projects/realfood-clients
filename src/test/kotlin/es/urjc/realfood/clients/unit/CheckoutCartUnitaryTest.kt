708e191ba14ead4c2c79eb1fe675f9ef8a9fb6cc
cristoflop <cristofer.lc.99@gmail.com>
2021-12-12 23:46:42 +0100

    Add checkout use case unitary tests

---
+++ src/test/kotlin/es/urjc/realfood/clients/unit/CheckoutCartUnitaryTest.kt
@@ -0,0 +1,60 @@
+package es.urjc.realfood.clients.unit
+
+import es.urjc.realfood.clients.application.CheckoutCartTest
+import es.urjc.realfood.clients.domain.exception.EntityNotFoundException
+import es.urjc.realfood.clients.domain.exception.ProductException
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.`when`
+
+class CheckoutCartUnitaryTest : CheckoutCartTest() {
+
+    @Test
+    fun `given valid request when checkout user cart then return order id`() {
+        `when`(cartRepository.findByClientId(validClientId()))
+            .thenReturn(validCart())
+        `when`(checkoutCartService(validCheckoutServiceRequest()))
+            .thenReturn(validCheckoutServiceResponse())
+
+        val response = checkoutCart(validCheckoutCartRequest())
+
+        assertEquals(validOrderId().toString(), response.orderId)
+    }
+
+    @Test
+    fun `given valid request and error status from external checkout when checkout user cart then return product exception`() {
+        `when`(cartRepository.findByClientId(validClientId()))
+            .thenReturn(validCart())
+        `when`(checkoutCartService(validCheckoutServiceRequest()))
+            .thenReturn(invalidCheckoutServiceResponse())
+
+        val exc = Assertions.assertThrows(ProductException::class.java) {
+            checkoutCart(validCheckoutCartRequest())
+        }
+
+        assertEquals("Error from Restaurants API", exc.message)
+    }
+
+    @Test
+    fun `given invalid client id when checkout user cart then return illegal argument exception`() {
+        val exc = Assertions.assertThrows(IllegalArgumentException::class.java) {
+            checkoutCart(invalidCheckoutCartRequest())
+        }
+
+        Assertions.assertTrue(exc.message!!.contains("Invalid UUID"))
+    }
+
+    @Test
+    fun `given valid request but nonexistent user when checkout user cart then return entity not found exception`() {
+        `when`(cartRepository.findByClientId(validClientId()))
+            .thenReturn(null)
+
+        val exc = Assertions.assertThrows(EntityNotFoundException::class.java) {
+            checkoutCart(validCheckoutCartRequest())
+        }
+
+        assertEquals("Cart not found", exc.message)
+    }
+
+}
