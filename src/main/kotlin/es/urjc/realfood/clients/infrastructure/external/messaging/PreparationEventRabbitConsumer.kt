96409c4a4b25e00c47dadd1847bd525d4716d905
cristoflop <cristofer.lc.99@gmail.com>
2022-05-18 19:00:00 +0200

    Add PreparationEvent consumer

---
+++ src/main/kotlin/es/urjc/realfood/clients/infrastructure/external/messaging/PreparationEventRabbitConsumer.kt
@@ -0,0 +1,39 @@
+package es.urjc.realfood.clients.infrastructure.external.messaging
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import es.urjc.realfood.clients.application.UpdateOrderStatus
+import es.urjc.realfood.clients.application.UpdateOrderStatusRequest
+import es.urjc.realfood.clients.domain.Status
+import org.slf4j.LoggerFactory
+import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.stereotype.Component
+
+@Component
+class PreparationEventRabbitConsumer(
+    private val updateOrderStatus: UpdateOrderStatus
+) {
+
+    private val logger = LoggerFactory.getLogger(PaymentEventRabbitConsumer::class.java)
+
+    private val objectMapper = ObjectMapper()
+
+    @RabbitListener(queues = ["prepared-orders"], ackMode = "AUTO")
+    private fun consume(message: String) {
+        val preparedEvent = objectMapper.readValue(message, PreparedEvent::class.java)
+        updateOrderStatus(
+            UpdateOrderStatusRequest(
+                clientId = preparedEvent.clientId,
+                orderId = preparedEvent.orderId,
+                status = Status.PREPARED
+            )
+        )
+
+        logger.info("[Consumed] prepared order '{}'", preparedEvent.orderId)
+    }
+
+}
+
+data class PreparedEvent(
+    val clientId: String,
+    val orderId: String
+)
