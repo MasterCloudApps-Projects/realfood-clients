c5fb69691ac8643d0ad28a3d6099080a56dae557
cristoflop <cristofer.lc.99@gmail.com>
2022-05-18 20:01:06 +0200

    Add ShipmentEvent consumer

---
+++ src/main/kotlin/es/urjc/realfood/clients/infrastructure/external/messaging/ShipmentEventRabbitConsumer.kt
@@ -0,0 +1,43 @@
+package es.urjc.realfood.clients.infrastructure.external.messaging
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import es.urjc.realfood.clients.application.UpdateOrderStatus
+import es.urjc.realfood.clients.application.UpdateOrderStatusRequest
+import es.urjc.realfood.clients.domain.Status
+import org.slf4j.LoggerFactory
+import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.stereotype.Component
+
+@Component
+class ShipmentEventRabbitConsumer(
+    private val updateOrderStatus: UpdateOrderStatus
+) {
+
+    private val logger = LoggerFactory.getLogger(ShipmentEventRabbitConsumer::class.java)
+
+    private val objectMapper = ObjectMapper()
+
+    @RabbitListener(queues = ["sent-orders"], ackMode = "AUTO")
+    private fun consume(message: String) {
+        val sentEvent = objectMapper.readValue(message, SentEvent::class.java)
+        updateOrderStatus(
+            UpdateOrderStatusRequest(
+                clientId = sentEvent.clientId,
+                orderId = sentEvent.orderId,
+                status = Status.IN_DELIVERY
+            )
+        )
+
+        logger.info(
+            "[Consumed] sent order '{}' to client '{}'",
+            sentEvent.orderId,
+            sentEvent.clientId
+        )
+    }
+
+}
+
+data class SentEvent(
+    val clientId: String,
+    val orderId: String
+)
