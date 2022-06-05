ca5ec0e701f3bf88949df72447a0511ce9b99803
cristoflop <cristofer.lc.99@gmail.com>
2022-06-05 13:36:22 +0200

    Add delete client event publisher

---
+++ src/main/kotlin/es/urjc/realfood/clients/infrastructure/external/messaging/DeleteClientEventRabbitPublisher.kt
@@ -0,0 +1,25 @@
+package es.urjc.realfood.clients.infrastructure.external.messaging
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import es.urjc.realfood.clients.domain.services.DeleteClientEvent
+import es.urjc.realfood.clients.domain.services.DeleteClientEventPublisher
+import org.slf4j.LoggerFactory
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+
+class DeleteClientEventRabbitPublisher(
+    private val rabbitTemplate: RabbitTemplate
+) : DeleteClientEventPublisher {
+
+    private val logger = LoggerFactory.getLogger(DeleteClientEventRabbitPublisher::class.java)
+
+    private val objectMapper: ObjectMapper = ObjectMapper()
+
+    private val queueName: String = "delete-client-queue"
+
+    override fun invoke(deleteClientEvent: DeleteClientEvent) {
+        val msg: String = objectMapper.writeValueAsString(deleteClientEvent)
+        rabbitTemplate.convertAndSend(queueName, msg)
+        logger.info("[Published] delete client request sent for user: {}", deleteClientEvent.clientId)
+    }
+
+}
